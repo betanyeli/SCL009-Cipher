cdd59c6dc3cfccf46b544677c6b032f5989128a0
betanyeli <betangelii@gmail.com>
2019-04-08 13:10:16 -0400

    Cambio en la funcion cipher de los espacios y caracteres especiales

--- src/cipher.js
+++ src/cipher.js
@@ -13,31 +13,42 @@ window.cipher = {
         let asciiLetterMinuscula = String.fromCharCode(asciiMinuscula); //Pasar a letras normales
         msj += asciiLetterMinuscula; //Concatena el mensake
       }
-      else if (text.charCodeAt(index) === 32) { // si la iteración encuentra un space sera true y si es así, lo imprima.
-        msj += " ";
+      else if (textToAscii >= 33 && textToAscii <= 64) { //condicion con los límites de caracteres especiales y numeros
+        let asciiNumber = (textToAscii - 33 + parseInt(offset)) % 32 + 33; // formula cipher con caracteres especiales
+        let asciiNumberNormal = String.fromCharCode(asciiNumber); //Pasar a letras normales
+        msj += asciiNumberNormal; //Concatena el mensake 
+      }
+ else {
+        msj += text[index]; // si encuentra un caracter no definido en límites, dejar igualito.
+      }
+    }
+    return msj; //retorna el mensaje
+  },
+  decode: (text, offset) => { // texto y desplazamiento
+    let msj = ""; // declarar el nombre de la función para que guarde el texto, por el momento vacío.
+    for (let index = 0; index < text.length; index++) { // Inicio del bucle que pasará por cada dato del texto.
+      let textToAscii = text.charCodeAt(index); //obtener codigo ascii
+      if (textToAscii >= 65 && textToAscii <= 90) { //condicion con los límites de mayúsculas
+        let asciiMayuscula = (textToAscii + 65 - parseInt(offset)) % 26 + 65; // formula decipher
+        let asciiLetterMayuscula = String.fromCharCode(asciiMayuscula); //Pasar a letras normales
+        msj += asciiLetterMayuscula; //concateno el mensaje
+      }
+      else if (textToAscii >= 97 && textToAscii <= 122) { //condicion con los límites de minusculas
+        let asciiMinuscula = (textToAscii - 122 - parseInt(offset)) % 26 + 122; // formula decode
+        let asciiLetterMinuscula = String.fromCharCode(asciiMinuscula); //Pasar a letras normales
+        msj += asciiLetterMinuscula; //concateno el mensaje
+      }
+      else if (textToAscii >= 33 && textToAscii <= 64) { //condicion con los límites de caracteres especiales y numeros
+        let asciiNumber = (textToAscii - 64 - parseInt(offset)) % 32 + 64; // formula cipher
+        let asciiNumberNormal = String.fromCharCode(asciiNumber); //Pasar a letras normales
+        msj += asciiNumberNormal; //Concatena el mensake 
+      }
+
+    else {
+        msj += text[index]; // si encuentra un caracter no definido en límites, dejar igualito.
       }
 
     }
-  return msj; //retorna el mensaje
-},
-decode: (text, offset) => { // texto y desplazamiento
-  let msj = ""; // declarar el nombre de la función para que guarde el texto, por el momento vacío.
-  for (let index = 0; index < text.length; index++) { // Inicio del bucle que pasará por cada dato del texto.
-    let textToAscii = text.charCodeAt(index); //obtener codigo ascii
-    if (textToAscii >= 65 && textToAscii <= 90) { //condicion con los límites de mayúsculas
-      let asciiMayuscula = (textToAscii + 65 - parseInt(offset)) % 26 + 65; // formula decipher
-      let asciiLetterMayuscula = String.fromCharCode(asciiMayuscula); //Pasar a letras normales
-      msj += asciiLetterMayuscula; //concateno el mensaje
-    }
-    else if (textToAscii >= 97 && textToAscii <= 122) { //condicion con los límites de minusculas
-      let asciiMinuscula = (textToAscii - 122 - parseInt(offset)) % 26 + 122; // formula decode
-      let asciiLetterMinuscula = String.fromCharCode(asciiMinuscula); //Pasar a letras normales
-      msj += asciiLetterMinuscula; //concateno el mensaje
-    }
-    else if (text.charCodeAt(index) === 32) { // si la iteración encuentra un space sera true y si es así, lo imprima.
-      msj += " ";
-    }
-
+    return msj; // retorna el resultado
   }
- return msj;
-} }
+}
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -11,11 +11,18 @@ describe('cipher', () => {
     });
 
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
-assert.equal(cipher.encode('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 33),'HIJKLMNOPQRSTUVWXYZABCDEFG' );
+      assert.equal(cipher.encode('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 33), 'HIJKLMNOPQRSTUVWXYZABCDEFG');
     });
     it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () => {
-      assert.equal(cipher.encode('abcdefghijklmnopqrstuvwxyz', 33),'hijklmnopqrstuvwxyzabcdefg' );
+      assert.equal(cipher.encode('abcdefghijklmnopqrstuvwxyz', 33), 'hijklmnopqrstuvwxyzabcdefg');
+    });
+    it('debería retornar "Nsbjvzh lu zhunyl; 261&" para "Glucosa en sangre: 150%" con offset 33', () => {
+      assert.equal(cipher.encode('Glucosa en sangre: 150%', 33), 'Nsbjvzh lu zhunyl; 261&');
+    });
+    it('debería retornar "ÑHÑH ñhñh" para "ÑAÑA ñaña", con offset 33', () => {
+      assert.equal(cipher.encode('ÑAÑA ñaña', 33),'ÑHÑH ñhñh' );
           });
+
   });
 
   describe('cipher.decode', () => {
@@ -25,11 +32,17 @@ assert.equal(cipher.encode('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 33),'HIJKLMNOPQRSTUVWXY
     });
 
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
-      assert.equal(cipher.decode('HIJKLMNOPQRSTUVWXYZABCDEFG', 33),'ABCDEFGHIJKLMNOPQRSTUVWXYZ'  );
+      assert.equal(cipher.decode('HIJKLMNOPQRSTUVWXYZABCDEFG', 33), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     });
     it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
-      assert.equal(cipher.decode('hijklmnopqrstuvwxyzabcdefg', 33),'abcdefghijklmnopqrstuvwxyz' );
-          });
+      assert.equal(cipher.decode('hijklmnopqrstuvwxyzabcdefg', 33), 'abcdefghijklmnopqrstuvwxyz');
+    });
+    it('debería retornar "Glucosa en sangre: 150%" para "Nsbjvzh lu zhunyl; 261&", con offset 33', () => {
+    assert.equal(cipher.decode('Nsbjvzh lu zhunyl; 261&', 33), 'Glucosa en sangre: 150%');
   });
+  it('debería retornar "ÑAÑA ñaña" para "ÑHÑH ñhñh", con offset 33', () => {
+    assert.equal(cipher.decode('ÑHÑH ñhñh', 33),'ÑAÑA ñaña' );
+        });
+});
 
 });
