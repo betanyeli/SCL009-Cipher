f61c1f5454cd45d5a63ff2b536295e7e0bcf39fc
betanyeli <betangelii@gmail.com>
2019-04-08 21:01:30 -0400

    Agregar bootstrap, cambios en html, css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 window.onload = () => {
-
+ 
+  //----------------CIFRADO
   let btn_cipher = document.getElementById("ciph"); //declarar la variable del botón y llamarlo.
   btn_cipher.addEventListener("click", () => {  //Función del botón click
     let offsetCipher = document.getElementById("desplazamiento").value; //Declarar el valor del offset.
